68c95dfe46aeff85585044789c699781d8ecfce4
Natasha <a1@MacBook-Pro-Natasha.local>
2022-07-22 19:56:34 +0300

    gitignore added and modified

---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет 12821 -> да 23432 -> да
+void palindrom()
+        {
+        Console.Write("Введите натуральное пятизначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine()); 
+        int num1 = number / 10000 % 10;
+        int num2 = number / 1000 % 10;
+        int rev1 = number / 10 % 10;
+        int rev2 = number % 10;
+        int nul = number / 10000;
+        
+            if (nul < 1 || nul > 9){
+            Console.WriteLine(number + " -> " + "Нет, неправильный ввод числа");
+            }
+            else if  (num1 == rev2 && num2 == rev1){
+            Console.WriteLine(number + " -> Да, это палиндром");
+            }
+            else{
+            Console.WriteLine(number + " -> Нет, это не палиндром");
+            }
+        }
+{ 
+    palindrom();
+}
